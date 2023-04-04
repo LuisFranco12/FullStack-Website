e58923683979df759411e94ab542a88416258b09
LuisFranco12 <luisf3525@gmail.com>
2023-04-03 20:26:24 -0400

    added styles to form

--- FullStack/client/src/components/ShowStory.js
+++ FullStack/client/src/components/ShowStory.js
@@ -82,100 +82,107 @@ const ShowStory = ({user}) => {
 
     if(story) {
         return ( 
-            <div className='show-page'>
-                <div className='show-view-top'> 
-                    <div className='show-view-image-container'>
-                        <img src={story.image} alt={story.title + " image"}/>
-                    </div>
-                    <div className='show-view-right'>
-                        <h2>{story.title}</h2>
-                        <p id='show-view-author'>Author: {story.author}</p>
-                        <div>
-                            <h5>Synopsis</h5>
-                            <p id='synopsis'>{story.synopsis}</p>
+            <div className='show-page-container'>
+                <div className='show-page'>
+                    <div className='show-view-top'> 
+                        <div className='show-view-image-container'>
+                            <img src={story.image} alt={story.title + " image"}/>
                         </div>
-                        <p> <span>Genre:</span> {story.genre} </p>
+                        <div className='show-view-right'>
+                            <h2>{story.title}</h2>
+                            <p id='show-view-author'>Author: {story.author}</p>
+                            <div>
+                                <h5>Synopsis</h5>
+                                <p id='synopsis'>{story.synopsis}</p>
+                            </div>
+                            <p> <span>Genre:</span> {story.genre} </p>
 
-                        <div className='read-edit-delete'>
-                            <button id='read'>Read</button>
+                            <div className='read-edit-delete'>
+                                <button id='read'>Read</button>
+                            {
+                            story.author === user ?
+                                <>
+                                    <Link id="edit" style={{textDecoration: "none"}} to={`/story/edit/${story._id}`}>
+                                        Edit
+                                    </Link>
+                                    <button onClick={deleteStory} id='delete'><RiDeleteBin6Line /></button>
+                                </>
+                            :
+                                ""
+                            }
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <h3>Leave a Review!</h3>
+                    <div className='review-form-container'>
                         {
-                        story.author === user ?
+                            user &&
+                                <form className='review-form' onSubmit={leaveAReview}>
+                                    <div className='review-title-container'>
+                                        <label htmlFor="R-ttl">Review Title</label> <br />
+                                        <input type="text" id="R-ttl" name="title" ref={title}/> <br />
+                                    </div>
+        
+                                    <div className='review-content-container'>
+                                        <label htmlFor="R-bdy">Review Content</label> <br />
+                                        <textarea name="body" id="R-bdy" cols="30" rows="3" ref={content}/> <br />
+                                    </div>
+        
+                                    {/* add a rating */}
+                    
+                                    <button>Review</button>
+                                </form>
+                        } 
+                    </div>
+        
+                    {/* <div>
+                    
+        
+                        {
+                            story.reviews?.length <= 0 ?
                             <>
-                                <Link id="edit" style={{textDecoration: "none"}} to={`/story/edit/${story._id}`}>
-                                    Edit
-                                </Link>
-                                <button onClick={deleteStory} id='delete'><RiDeleteBin6Line /></button>
+                                Be the first to leave a review!
                             </>
                         :
-                            ""
+                            <>
+                                {reviewLength} review(s)
+                            </>
                         }
-                        </div>
 
-                    </div>
-                </div>
-
-                <div className='review-form-container'>
-                    {
-                        user &&
-                            <form className='review-form' onSubmit={leaveAReview}>
-                                <label htmlFor="R-ttl">Review Title</label> <br />
-                                <input type="text" id="R-ttl" name="title" ref={title}/> <br />
-    
-                                <label htmlFor="R-bdy">Review Content</label> <br />
-                                <textarea name="body" id="R-bdy" cols="30" rows="3" ref={content}/> <br />
-    
-                                {/* add a rating */}
-                 
-                                <button>Review</button>
-                            </form>
-                    } 
-                </div>
-    
-                {/* <div>
-                   
-    
-                    {
-                        story.reviews?.length <= 0 ?
-                        <>
-                            Be the first to leave a review!
-                        </>
-                    :
-                        <>
-                            {reviewLength} review(s)
-                        </>
-                    }
-
-                    {
-                        story.reviews?.map(review => (
-                            <div>
-                                <p>{review.title}</p>
-                                <p>reviewer: {review.reviewer}</p>
-                                <p>rating: {review.rating}</p>
-                                <p>{review.body}</p>
-                                <p>likes: {review.likes}</p>
+                        {
+                            story.reviews?.map(review => (
                                 <div>
-                                    {
-                                        review.reviewer === user ?
-                                            <>
-                                                <button onClick={() => deleteReview(review)}>Delete Review</button>
-                                                <Link to={`/story/review/edit/${story._id}/${review._id}`}>
-                                                    edit review
-                                                </Link>
-                                            </>
-                                        :
-                                            ''
-                                    }
+                                    <p>{review.title}</p>
+                                    <p>reviewer: {review.reviewer}</p>
+                                    <p>rating: {review.rating}</p>
+                                    <p>{review.body}</p>
+                                    <p>likes: {review.likes}</p>
+                                    <div>
+                                        {
+                                            review.reviewer === user ?
+                                                <>
+                                                    <button onClick={() => deleteReview(review)}>Delete Review</button>
+                                                    <Link to={`/story/review/edit/${story._id}/${review._id}`}>
+                                                        edit review
+                                                    </Link>
+                                                </>
+                                            :
+                                                ''
+                                        }
+                                    </div>
                                 </div>
-                            </div>
-                        ))
-                    }
-    
+                            ))
+                        }
+        
+                    </div>
+        
+                    <br />
+        
+                    */} */}
+        
                 </div>
-    
-                <br />
-    
-                 */} */}
-    
             </div>
          );
     }
